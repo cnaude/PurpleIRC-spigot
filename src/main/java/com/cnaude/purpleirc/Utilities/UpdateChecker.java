dc9626d2590569df2dbf7f346a8d730cc619e7e0
cnaude <cnaude@zebra>
2015-01-01 13:40:30 -0700

    New Spigot only version of PurpleIRC.

--- src/main/java/com/cnaude/purpleirc/Utilities/UpdateChecker.java
+++ src/main/java/com/cnaude/purpleirc/Utilities/UpdateChecker.java
@@ -68,12 +68,12 @@ public class UpdateChecker {
             }
         }, 0, 432000);
     }
-    
+
     public void asyncUpdateCheck(final CommandSender sender, final String mode) {
         plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, new Runnable() {
             @Override
             public void run() {
-                if (plugin.isUpdateCheckerEnabled()) {                    
+                if (plugin.isUpdateCheckerEnabled()) {
                     updateCheck(sender, mode);
                 }
             }
@@ -81,65 +81,67 @@ public class UpdateChecker {
     }
 
     private void updateCheck(CommandSender sender, String mode) {
-        if (mode.equalsIgnoreCase("stable")) {
-            try {
-                URL url = new URL("https://api.curseforge.com/servermods/files?projectids=56773");
-                URLConnection conn = url.openConnection();
-                conn.setReadTimeout(5000);
-                conn.addRequestProperty("User-Agent", "PurpleIRC Update Checker");
-                conn.setDoOutput(true);
-                final BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                final String response = reader.readLine();
-                final JSONArray array = (JSONArray) JSONValue.parse(response);
-                if (array.size() == 0) {
-                    plugin.logInfo("No files found, or Feed URL is bad.");
-                    return;
-                }
-                newVersion = ((String) ((JSONObject) array.get(array.size() - 1)).get("name")).trim();
-                plugin.logDebug("newVersionTitle: " + newVersion);
-                newBuild = Integer.valueOf(newVersion.split("-")[1]);
-                if (newBuild > currentBuild) {
-                    sender.sendMessage(plugin.LOG_HEADER_F + " Stable version: " + newVersion + " is out!" + " You are still running version: " + currentVersion);
-                    sender.sendMessage(plugin.LOG_HEADER_F + " Update at: http://dev.bukkit.org/server-mods/purpleirc");
-                } else if (currentBuild > newBuild) {
-                    sender.sendMessage(plugin.LOG_HEADER_F + " Stable version: " + newVersion + " | Current Version: " + currentVersion);
-                } else {
-                    sender.sendMessage(plugin.LOG_HEADER_F + " No new version available");
-                }
-            } catch (IOException | NumberFormatException e) {
-                sender.sendMessage(plugin.LOG_HEADER_F + " Error checking for latest version: " + e.getMessage());
+        /*
+         if (mode.equalsIgnoreCase("stable")) {
+         try {
+         URL url = new URL("https://api.curseforge.com/servermods/files?projectids=56773");
+         URLConnection conn = url.openConnection();
+         conn.setReadTimeout(5000);
+         conn.addRequestProperty("User-Agent", "PurpleIRC Update Checker");
+         conn.setDoOutput(true);
+         final BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+         final String response = reader.readLine();
+         final JSONArray array = (JSONArray) JSONValue.parse(response);
+         if (array.size() == 0) {
+         plugin.logInfo("No files found, or Feed URL is bad.");
+         return;
+         }
+         newVersion = ((String) ((JSONObject) array.get(array.size() - 1)).get("name")).trim();
+         plugin.logDebug("newVersionTitle: " + newVersion);
+         newBuild = Integer.valueOf(newVersion.split("-")[1]);
+         if (newBuild > currentBuild) {
+         sender.sendMessage(plugin.LOG_HEADER_F + " Stable version: " + newVersion + " is out!" + " You are still running version: " + currentVersion);
+         sender.sendMessage(plugin.LOG_HEADER_F + " Update at: http://dev.bukkit.org/server-mods/purpleirc");
+         } else if (currentBuild > newBuild) {
+         sender.sendMessage(plugin.LOG_HEADER_F + " Stable version: " + newVersion + " | Current Version: " + currentVersion);
+         } else {
+         sender.sendMessage(plugin.LOG_HEADER_F + " No new version available");
+         }
+         } catch (IOException | NumberFormatException e) {
+         sender.sendMessage(plugin.LOG_HEADER_F + " Error checking for latest version: " + e.getMessage());
+         }
+         } else {
+         */
+        try {
+            URL url = new URL("http://h.cnaude.org:8081/job/PurpleIRC-spigot/lastStableBuild/api/json");
+            URLConnection conn = url.openConnection();
+            conn.setReadTimeout(5000);
+            conn.addRequestProperty("User-Agent", "PurpleIRC-spigot Update Checker");
+            conn.setDoOutput(true);
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            final String response = reader.readLine();
+            final JSONObject obj = (JSONObject) JSONValue.parse(response);
+            if (obj.isEmpty()) {
+                sender.sendMessage(plugin.LOG_HEADER_F + " No files found, or Feed URL is bad.");
+                return;
             }
-        } else {
-            try {
-                URL url = new URL("http://h.cnaude.org:8081/job/PurpleIRC/lastStableBuild/api/json");
-                URLConnection conn = url.openConnection();
-                conn.setReadTimeout(5000);
-                conn.addRequestProperty("User-Agent", "PurpleIRC Update Checker");
-                conn.setDoOutput(true);
-                final BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                final String response = reader.readLine();
-                final JSONObject obj = (JSONObject) JSONValue.parse(response);
-                if (obj.isEmpty()) {
-                    sender.sendMessage(plugin.LOG_HEADER_F + " No files found, or Feed URL is bad.");
-                    return;
-                }
 
-                newVersion = obj.get("number").toString();
-                String downloadUrl = obj.get("url").toString();
-                plugin.logDebug("newVersionTitle: " + newVersion);
-                newBuild = Integer.valueOf(newVersion);
-                if (newBuild > currentBuild) {
-                    sender.sendMessage(plugin.LOG_HEADER_F + " Latest dev build: " + newVersion + " is out!" + " You are still running build: " + currentVersion);
-                    sender.sendMessage(plugin.LOG_HEADER_F + " Update at: " + downloadUrl);
-                } else if (currentBuild > newBuild) {
-                    sender.sendMessage(plugin.LOG_HEADER_F + " Dev build: " + newVersion + " | Current build: " + currentVersion);
-                } else {
-                    sender.sendMessage(plugin.LOG_HEADER_F + " No new version available");
-                }
-            } catch (IOException | NumberFormatException e) {
-                sender.sendMessage(plugin.LOG_HEADER_F + " Error checking for latest dev build: " + e.getMessage());
+            newVersion = obj.get("number").toString();
+            String downloadUrl = obj.get("url").toString();
+            plugin.logDebug("newVersionTitle: " + newVersion);
+            newBuild = Integer.valueOf(newVersion);
+            if (newBuild > currentBuild) {
+                sender.sendMessage(plugin.LOG_HEADER_F + " Latest dev build: " + newVersion + " is out!" + " You are still running build: " + currentVersion);
+                sender.sendMessage(plugin.LOG_HEADER_F + " Update at: " + downloadUrl);
+            } else if (currentBuild > newBuild) {
+                sender.sendMessage(plugin.LOG_HEADER_F + " Dev build: " + newVersion + " | Current build: " + currentVersion);
+            } else {
+                sender.sendMessage(plugin.LOG_HEADER_F + " No new version available");
             }
+        } catch (IOException | NumberFormatException e) {
+            sender.sendMessage(plugin.LOG_HEADER_F + " Error checking for latest dev build: " + e.getMessage());
         }
+
     }
 
     public void cancel() {
