468c2474bac53cd4b8352599f83e44cb67ec0983
cnaude <cnaude@zebra.lan>
2015-06-23 19:19:22 -0700

    Dependency fixes and relocations.

--- src/main/java/com/cnaude/purpleirc/CommandHandlers.java
+++ src/main/java/com/cnaude/purpleirc/CommandHandlers.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import net.md_5.bungee.api.ChatColor;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
