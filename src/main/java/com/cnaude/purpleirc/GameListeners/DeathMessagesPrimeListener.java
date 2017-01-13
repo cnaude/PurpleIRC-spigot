bdca07f1cfd951b51116855cc1135466daf1e583
cnaude <cnaude@zebra.lan>
2017-01-12 18:25:46 -0700

    Reduce kick event spamming.

---
+++ src/main/java/com/cnaude/purpleirc/GameListeners/DeathMessagesPrimeListener.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2014 cnaude
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.cnaude.purpleirc.GameListeners;
+
+import com.cnaude.purpleirc.PurpleBot;
+import com.cnaude.purpleirc.PurpleIRC;
+import com.cnaude.purpleirc.TemplateName;
+import net.memmove.bukkit.deathmsg.DeathMessageBroadcastEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+/**
+ *
+ * @author Chris Naude
+ */
+public class DeathMessagesPrimeListener implements Listener {
+
+    private final PurpleIRC plugin;
+
+    /**
+     *
+     * @param plugin the PurpleIRC plugin
+     */
+    public DeathMessagesPrimeListener(PurpleIRC plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     *
+     * @param event
+     */
+    @EventHandler
+    public void onDeathMessageEvent(DeathMessageBroadcastEvent event) {
+        plugin.logDebug("onDeathMessageBroadcastEvent caught");
+        for (PurpleBot ircBot : plugin.ircBots.values()) {
+            ircBot.gameDeath(event.getPlayer(), event.getMessage().getText(), TemplateName.DEATH_MESSAGES);
+        }
+    }
+}
