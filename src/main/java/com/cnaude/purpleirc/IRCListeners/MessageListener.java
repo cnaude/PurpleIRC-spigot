ca6d00c7b95289c2df333666353fa5e7af0321f6
cnaude <cnaude@zebra.lan>
2017-06-01 19:20:27 -0700

    Slightly less verbose debugging.

--- src/main/java/com/cnaude/purpleirc/IRCListeners/MessageListener.java
+++ src/main/java/com/cnaude/purpleirc/IRCListeners/MessageListener.java
@@ -53,8 +53,9 @@ public class MessageListener extends ListenerAdapter {
         Channel channel = event.getChannel();
         User user = event.getUser();
 
-        plugin.logDebug("IRC message caught [channel: " + channel + "] [nick: " + user.getNick() + "] [[message: " + message + "]");
-        
+        plugin.logDebug("IRC message caught [channel: " + channel.getName()
+                + " (" + channel.getMode() + ")] [nick: " + user.getNick()
+                + "] [message: " + message + "]");
         try {
             if (plugin.shortifyHook != null && ircBot.isShortifyEnabled(channel.getName())) {
                 plugin.logDebug("Shortifying message (before): " + message);
